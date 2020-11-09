cb478b7124377d880d40c12422a50465f7d40a08
saarthk <saarthak19445@iiitd.ac.in>
2020-11-09 20:29:50 +0530

    added rotation component

---
+++ src/RotationComponent.java
@@ -0,0 +1,39 @@
+import javafx.scene.transform.Rotate;
+
+public class RotationComponent extends Component {
+    private double angularVelocity;
+    private Vector2D pivot;
+    private final Rotate rotateTransform;
+
+    public RotationComponent(double angularVelocity, double x, double y) {
+        this.angularVelocity = angularVelocity;
+        pivot = new Vector2D(x, y);
+        rotateTransform = new Rotate(0, x, y);
+    }
+
+    public double getAngularVelocity() {
+        return angularVelocity;
+    }
+
+    public Vector2D getPivot() {
+        return pivot;
+    }
+
+    public Rotate getRotateTransform() {
+        return rotateTransform;
+    }
+
+    public void setAngularVelocity(double angularVelocity) {
+        this.angularVelocity = angularVelocity;
+    }
+
+    public void setPivot(double x, double y) {
+        this.pivot = new Vector2D(x, y);
+        this.rotateTransform.setPivotX(x);
+        this.rotateTransform.setPivotY(y);
+    }
+
+    public void rotate(double timeInSeconds) {
+        rotateTransform.setAngle(angularVelocity * timeInSeconds);
+    }
+}
